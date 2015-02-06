da0bccb8e31984f8568ea37404f9e4bba0f187d1
luc <luc@imba.(none)>
2015-02-06 15:31:05 +0100

    cleaning up tidyup_executive

--- tidyup/include/tidyup/arms_at_side.h
+++ tidyup/include/tidyup/arms_at_side.h
@@ -3,6 +3,7 @@
 
 #include <ros/ros.h>
 #include <tidyup_msgs/ArmsAtSide.h>
+#include <moveit/move_group_interface/move_group.h>
 
 namespace tidyup
 {
@@ -10,11 +11,19 @@ namespace tidyup
 	class armsAtSide
 	{
 		public:
-		armsAtSide();
+		armsAtSide(moveit::planning_interface::MoveGroup* right_arm_group,
+				moveit::planning_interface::MoveGroup* left_arm_group);
 		~armsAtSide();
 
-		static bool checkIfArmsAtSide(tidyup_msgs::ArmsAtSide::Request &req,
+		bool checkIfArmsAtSide(tidyup_msgs::ArmsAtSide::Request &req,
 								tidyup_msgs::ArmsAtSide::Response &res);
+
+		// Load the joint values for the arm at the side into the given vector.
+		bool loadJointValues(const std::string &group_name, std::vector<double> &arm_joints) const;
+		private:
+
+		moveit::planning_interface::MoveGroup* right_arm_group_;
+		moveit::planning_interface::MoveGroup* left_arm_group_;
 
 	};
 
--- tidyup/src/tidyup.cpp
+++ tidyup/src/tidyup.cpp
@@ -4,14 +4,25 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "tidyup");
-  ros::NodeHandle nhPriv("~");
+	ros::init(argc, argv, "tidyup");
+	ros::NodeHandle nhPriv("~");
 
-  		moveit::planning_interface::MoveGroup right_arm_group("right_arm");
-  		moveit::planning_interface::MoveGroup left_arm_group("left_arm");
+	moveit::planning_interface::MoveGroup right_arm_group("right_arm");
+	moveit::planning_interface::MoveGroup left_arm_group("left_arm");
+	moveit::planning_interface::MoveGroup arms_group("arms");
 
-  ros::ServiceServer service = nhPriv.advertiseService("arms_at_side", tidyup::armsAtSide::checkIfArmsAtSide);
-  ros::spin();
+	// Needed to initialize/update moveGroups to the current state!
+	right_arm_group.getCurrentState();
+	left_arm_group.getCurrentState();
+	arms_group.getCurrentState();
 
-  return 0;
+	// Initialize objects.
+	tidyup::armsAtSide armsAtSide(&right_arm_group, &left_arm_group);
+
+	// Advertise the different services and action servers.
+	ros::ServiceServer service = nhPriv.advertiseService(
+			"arms_at_side", &tidyup::armsAtSide::checkIfArmsAtSide, &armsAtSide);
+	ros::spin();
+
+	return 0;
 }
