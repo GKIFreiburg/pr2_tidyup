c654a9a41762fc62e8fd2c2dd29be22fef01d07d
luc <luc@imba.(none)>
2015-03-10 18:18:42 +0100

    changed the topic name of the filtered kinect images to match with the topic name of christians ork_to_planning_scene

--- object_detection/filter_kinect.cpp
+++ object_detection/filter_kinect.cpp
@@ -3,7 +3,6 @@
 #include <sensor_msgs/Image.h>
 
 ros::Publisher *pubImgPtr;
-ros::Publisher *pubCameraInfoPtr;
 int* counterPtr;
 
 void cameraImageReceived(
@@ -21,36 +20,18 @@ void cameraImageReceived(
   pubImgPtr->publish(msgIn);
 }
 
-void cameraInfoReceived(
-  const sensor_msgs::CameraInfo& msgIn
-) {
-  if (msgIn.header.frame_id == "head_mount_kinect_rgb_optical_frame")
-    return;
-
-  pubCameraInfoPtr->publish(msgIn);
-}
-
 int main(int argc, char **argv) {
   ros::init(argc, argv, "filter_kinect");
   ros::NodeHandle nh;
 
   pubImgPtr = new ros::Publisher(
     nh.advertise<sensor_msgs::Image>(
-      "filtered_kinect/depth/image_raw",
-      1000));
-
-  pubCameraInfoPtr = new ros::Publisher(
-    nh.advertise<sensor_msgs::CameraInfo>(
-      "filtered_kinect/depth/camera_info",
+      "head_mount_kinect/depth/image_fixed",
       1000));
 
   ros::Subscriber sub = nh.subscribe(
     "head_mount_kinect/depth/image_raw", 1000,
     &cameraImageReceived);
-
-  ros::Subscriber subCameraInfo = nh.subscribe(
-    "head_mount_kinect/depth/camera_info", 1000,
-    &cameraInfoReceived);
 
   int counter = 0;
   counterPtr = &counter;
