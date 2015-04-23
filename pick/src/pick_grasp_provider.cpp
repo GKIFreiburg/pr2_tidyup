99885fb319388af57127159f7dde9369a1f8090a
Lanners <lannersluc@gmail.com>
2015-04-23 14:54:46 +0200

    test environment for grasping

--- pick/src/pick_grasp_provider.cpp
+++ pick/src/pick_grasp_provider.cpp
@@ -55,6 +55,7 @@
 #include <object_recognition_msgs/RecognizedObjectArray.h>
 
 #include <tidyup_utils/stringutil.h>
+#include <ork_to_planning_scene_msgs/UpdatePlanningSceneFromOrkAction.h>
 #include <grasp_provider_msgs/GenerateGraspsAction.h>
 
 #include <tf/transform_listener.h>
@@ -64,66 +65,6 @@
 #include <rosbag/message_instance.h>
 #include <boost/foreach.hpp>
 #define forEach BOOST_FOREACH
-
-
-const std::string bagTopic = "grasp";
-const std::string bagFile = "grasp.txt";
-const std::string objectName = "coke";
-
-void pick(moveit::planning_interface::MoveGroup &group)
-{
-	std::vector<moveit_msgs::Grasp> grasps;
-
-	moveit_msgs::Grasp g;
-
-	geometry_msgs::PoseStamped p;
-	p.header.frame_id = "base_link";
-	p.header.stamp = ros::Time::now();
-	//p.pose.position.x = 0.25;
-	p.pose.position.x = 0.32; // 0.5-0.18 very important!
-	p.pose.position.y = -0.75;
-	p.pose.position.z = 0.58;
-	p.pose.orientation.x = 0;
-	p.pose.orientation.y = 0;
-	p.pose.orientation.z = 0;
-	p.pose.orientation.w = 1;
-	g.grasp_pose = p;
-	g.grasp_quality = 0.9;
-	//g.id = "Grasp" + boost::lexical_cast<std::string>(k);
-	//g.id = "Grasp" + boost::lexical_cast<std::string>(i) + " - " + boost::lexical_cast<std::string>(j);
-
-	g.pre_grasp_approach.direction.vector.x = 1.0;
-	g.pre_grasp_approach.direction.header.frame_id = "r_wrist_roll_link";
-	g.pre_grasp_approach.min_distance = 0.12;
-	g.pre_grasp_approach.desired_distance = 0.2;
-
-	g.post_grasp_retreat.direction.header.frame_id = "base_footprint";
-	g.post_grasp_retreat.direction.vector.z = 1.0;
-	g.post_grasp_retreat.min_distance = 0.12;
-	g.post_grasp_retreat.desired_distance = 0.2;
-
-	g.pre_grasp_posture.joint_names.resize(1, "r_gripper_motor_screw_joint");
-	//g.pre_grasp_posture.joint_names.resize(1, "r_gripper_joint");
-	//g.pre_grasp_posture.joint_names.resize(1, "r_gripper_l_finger_joint");
-	g.pre_grasp_posture.points.resize(1);
-	g.pre_grasp_posture.points[0].positions.resize(1);
-	g.pre_grasp_posture.points[0].positions[0] = 1.0;
-	g.pre_grasp_posture.points[0].time_from_start = ros::Duration(45.0);
-
-	g.grasp_posture.joint_names.resize(1, "r_gripper_motor_screw_joint");
-	//g.grasp_posture.joint_names.resize(1, "r_gripper_joint");
-	g.grasp_posture.points.resize(1);
-	g.grasp_posture.points[0].positions.resize(1);
-	g.grasp_posture.points[0].positions[0] = 0;
-	g.grasp_posture.points[0].time_from_start = ros::Duration(45.0);
-
-
-	grasps.push_back(g);
-
-	//group.setSupportSurfaceName("table");
-
-	group.pick(objectName, grasps);
-}
 
 void place(moveit::planning_interface::MoveGroup &group)
 {
@@ -176,19 +117,33 @@ void place(moveit::planning_interface::MoveGroup &group)
 	group.setPathConstraints(constr);
 	group.setPlannerId("RRTConnectkConfigDefault");
 
-	group.place(objectName, loc);
+//	group.place(objectName, loc);
 }
 
+void dumpGrasps(const std::vector<moveit_msgs::Grasp>& grasps)
+{
+	ROS_WARN("pick_grasp_provider: Dumping grasps into ~/dumpGrasps/");
+	std::ofstream outputFile;
+	for (size_t i = 0; i < grasps.size(); i++)
+	{
+		std::string fileName = "grasp";
+		fileName.append(boost::lexical_cast<std::string>(i) + ".txt");
+		ROS_WARN("pick_grasp_provider: FileName: %s", fileName.c_str());
+		outputFile.open(fileName.c_str());
+		outputFile << grasps[i];
+		outputFile.close();
+	}
+}
 
 // Publishing a coke, estimated by a block to the planning scene.
 // Pose w.r.t. base_link
-bool publishCokeBlockToPlanningScene(const ros::Publisher &pub_co, const geometry_msgs::Pose poseCokeBlock)
+void publishCokeBlockToPlanningScene(const ros::Publisher &pub_co, const geometry_msgs::Pose poseCokeBlock, const std::string& objName)
 {
 	moveit_msgs::CollisionObject co;
 	co.header.stamp = ros::Time::now();
 	co.header.frame_id = "base_link";
 
-	co.id = objectName;
+	co.id = objName;
 	co.primitives.resize(1);
 	co.primitives[0].type = shape_msgs::SolidPrimitive::BOX;
 	co.primitives[0].dimensions.resize(shape_tools::SolidPrimitiveDimCount<shape_msgs::SolidPrimitive::BOX>::value);
@@ -204,9 +159,32 @@ bool publishCokeBlockToPlanningScene(const ros::Publisher &pub_co, const geometr
 
 	co.operation = co.ADD;
 	pub_co.publish(co);
-
-	return true;
 }
+
+void publishTableToPlanningScene(const ros::Publisher& pub_co)
+{
+	moveit_msgs::CollisionObject co;
+	co.header.stamp = ros::Time::now();
+	co.header.frame_id = "odom_combined";
+
+	co.id = "table";
+	co.primitives.resize(1);
+	co.primitives[0].type = shape_msgs::SolidPrimitive::BOX;
+	co.primitives[0].dimensions.resize(shape_tools::SolidPrimitiveDimCount<shape_msgs::SolidPrimitive::BOX>::value);
+	// values from gazebo_worlds/objects/table/urdf.xacro
+	co.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_X] = 1.0;
+	co.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Y] = 2.0;
+	co.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Z] = 0.05;
+	co.primitive_poses.resize(1);
+	co.primitive_poses[0].position.x = 0.98;
+	co.primitive_poses[0].position.y = 0;
+	co.primitive_poses[0].position.z = 0.525;
+	co.primitive_poses[0].orientation.w = 1;
+
+	co.operation = co.ADD;
+	pub_co.publish(co);
+}
+
 
 int main(int argc, char **argv)
 {
@@ -214,33 +192,33 @@ int main(int argc, char **argv)
 	ros::AsyncSpinner spinner(1);
 	spinner.start();
 
-
 	ros::NodeHandle nh;
 	ros::Publisher pub_co = nh.advertise<moveit_msgs::CollisionObject>("collision_object", 10);
 
-	ROS_ERROR("Waiting for GetPlanningScene Service to start.");
+	ROS_INFO("Waiting for GetPlanningScene Service to start.");
 	ros::ServiceClient getPlanningSceneClient_;
 	getPlanningSceneClient_ = nh.serviceClient<moveit_msgs::GetPlanningScene>(move_group::GET_PLANNING_SCENE_SERVICE_NAME);
 	getPlanningSceneClient_.waitForExistence();
 
-	actionlib::SimpleActionClient<grasp_provider_msgs::GenerateGraspsAction> ac("generate_grasps", true);
-	ROS_ERROR("Waiting for action server to start.");
+	actionlib::SimpleActionClient<grasp_provider_msgs::GenerateGraspsAction> actionGenerateGrasps_("generate_grasps", true);
+	ROS_INFO("Waiting for action server \"generate_grasps\" to start.");
 	// wait for the action server to start
-	ac.waitForServer(); //will wait for infinite time
+	actionGenerateGrasps_.waitForServer(); // will wait for infinite time
 
-	ROS_ERROR("asdf");
-	ros::NodeHandle pnh;
-	bool object_detection_;
+	ros::NodeHandle pnh("~");
+	std::string object_detection_;
 	std::string grasp_object_;
 	std::string eef_name_;
-	pnh.param("object_detection", object_detection_, false);
+	// ros param: /pick_grasp_provider/object_detection
+	pnh.param<std::string>("object_detection", object_detection_, "true");
 	pnh.param<std::string>("grasp_object", grasp_object_, "coke");
 	pnh.param<std::string>("eef_name", eef_name_, "right_gripper");
+	ROS_WARN("object_detection is %s", object_detection_.c_str());
 
-	moveit::planning_interface::MoveGroup group("right_arm");
-	group.setPlanningTime(45.0);
+//	publishTableToPlanningScene(pub_co);
 
-	if (!object_detection_)
+
+	if (!(object_detection_.compare("true") == 0))
 	{
 		geometry_msgs::PoseStamped p;
 		p.header.frame_id = "base_link";
@@ -252,65 +230,95 @@ int main(int argc, char **argv)
 		p.pose.orientation.y = 0;
 		p.pose.orientation.z = 0;
 		p.pose.orientation.w = 1;
-		publishCokeBlockToPlanningScene(pub_co, p.pose);
-		ROS_INFO("Wait 2 sec after publishing cos");
-		ros::Duration(2.0).sleep();
+		publishCokeBlockToPlanningScene(pub_co, p.pose, "coke");
 
-	    moveit_msgs::GetPlanningScene::Request request;
-	    moveit_msgs::GetPlanningScene::Response response;
-	    request.components.components = moveit_msgs::PlanningSceneComponents::WORLD_OBJECT_GEOMETRY |
-	        moveit_msgs::PlanningSceneComponents::ROBOT_STATE_ATTACHED_OBJECTS;
-		if (!getPlanningSceneClient_.call(request, response))
+	} else { // using object detection
+
+		actionlib::SimpleActionClient<ork_to_planning_scene_msgs::UpdatePlanningSceneFromOrkAction> actionOrkToPs_("ork_to_planning_scene", true);
+		ROS_INFO("Waiting for action server \"ork_to_planning_scene\" to start.");
+		// wait for the action server to start
+		actionOrkToPs_.waitForServer(); // will wait for infinite time
+
+		// execute action ork-to-planning-scene
+		ROS_WARN("pick_grasp_provider: Sending UpdatePlanningSceneFromORK request.");
+		ork_to_planning_scene_msgs::UpdatePlanningSceneFromOrkGoal updatePSGoal;
+		updatePSGoal.verify_planning_scene_update = false;
+		updatePSGoal.add_tables = true;
+		actionOrkToPs_.sendGoal(updatePSGoal);
+		bool finished_before_timeout = actionOrkToPs_.waitForResult(ros::Duration(30.0));
+		if (finished_before_timeout)
 		{
-			ROS_ERROR("pick_grasp_provider: planning scene request failed.");
-			return 1;
-		}
-
-		moveit_msgs::CollisionObject collObj;
-		bool found = false;
-		ROS_ASSERT(response.scene.world.collision_objects.size() > 0);
-	    forEach(const moveit_msgs::CollisionObject & co, response.scene.world.collision_objects)
-	    {
-	    	ROS_ERROR("CO Name: %s", co.id.c_str());
-	    	if (StringUtil::startsWith(co.id, grasp_object_))
-	    	{
-	    		found = true;
-	    		collObj = co;
-	    		break;
-	    	}
-	    }
-
-	    if (!found)
-	    {
-	    	ROS_ERROR("pick_grasp_provider: No collision object found with name %s", grasp_object_.c_str());
-	    	return 1;
-	    }
-
-	    grasp_provider_msgs::GenerateGraspsGoal goal;
-	    goal.collision_object = collObj;
-	    goal.eef_group_name = eef_name_;
-	    ac.sendGoal(goal);
-
-	    //wait for the action to return
-	    bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
-
-	    if (finished_before_timeout)
-	    {
-			actionlib::SimpleClientGoalState state = ac.getState();
-			ROS_INFO("Action finished: %s",state.toString().c_str());
-			if(state != actionlib::SimpleClientGoalState::SUCCEEDED) {
-				ROS_ERROR("Generate grasp action failed.");
+			actionlib::SimpleClientGoalState state = actionOrkToPs_.getState();
+			ROS_INFO("pick_grasp_provider: ORK to Planning Scene Action finished: %s", state.toString().c_str());
+			if (state != actionlib::SimpleClientGoalState::SUCCEEDED)
+			{
+				ROS_ERROR("Update ORK to Planning Scene action failed.");
 				return 1;
 			}
-			std::vector<moveit_msgs::Grasp> grasps = ac.getResult()->grasps;
-//			grasp_provider_msgs::GenerateGraspsResultConstPtr a = ac.getResult();
+		} else {
+			ROS_ERROR("pick_grasp_provider: Update ORK to Planning Scene action did not finish before the time out.");
+			return 1;
+		}
+	}
 
-			group.pick(grasp_object_, grasps);
-	    } else {
-	        ROS_ERROR("pick_grasp_provider: Generate grasp action did not finish before the time out.");
-	        return 1;
-	    }
+	ROS_INFO("Wait 2 sec after publishing cos");
+	ros::Duration(2.0).sleep();
+	moveit_msgs::GetPlanningScene::Request request;
+	moveit_msgs::GetPlanningScene::Response response;
+	request.components.components = moveit_msgs::PlanningSceneComponents::WORLD_OBJECT_GEOMETRY |
+		moveit_msgs::PlanningSceneComponents::ROBOT_STATE_ATTACHED_OBJECTS;
+	if (!getPlanningSceneClient_.call(request, response))
+	{
+		ROS_ERROR("pick_grasp_provider: planning scene request failed.");
+		return 1;
+	}
 
+	moveit_msgs::CollisionObject collObj;
+	bool found = false;
+	ROS_ASSERT(response.scene.world.collision_objects.size() > 0);
+	forEach(const moveit_msgs::CollisionObject & co, response.scene.world.collision_objects)
+	{
+		ROS_INFO("CO Name: %s", co.id.c_str());
+		if (StringUtil::startsWith(co.id, grasp_object_))
+		{
+			found = true;
+			collObj = co;
+			break;
+		}
+	}
+
+	if (!found)
+	{
+		ROS_ERROR("pick_grasp_provider: No collision object found with name %s", grasp_object_.c_str());
+		return 1;
+	}
+
+	grasp_provider_msgs::GenerateGraspsGoal goal;
+	goal.collision_object = collObj;
+	goal.eef_group_name = eef_name_;
+	actionGenerateGrasps_.sendGoal(goal);
+
+	moveit::planning_interface::MoveGroup group("right_arm");
+	group.setPlanningTime(45.0);
+
+	//wait for the action to return
+	bool finished_before_timeout = actionGenerateGrasps_.waitForResult(ros::Duration(30.0));
+
+	if (finished_before_timeout)
+	{
+		actionlib::SimpleClientGoalState state = actionGenerateGrasps_.getState();
+		ROS_INFO("Action finished: %s",state.toString().c_str());
+		if(state != actionlib::SimpleClientGoalState::SUCCEEDED) {
+			ROS_ERROR("Generate grasp action failed.");
+			return 1;
+		}
+		std::vector<moveit_msgs::Grasp> grasps = actionGenerateGrasps_.getResult()->grasps;
+		ROS_INFO("pick_grasp_provider: %lu grasps were found.", grasps.size());
+		//dumpGrasps(grasps);
+		group.pick(grasp_object_, grasps);
+	} else {
+		ROS_ERROR("pick_grasp_provider: Generate grasp action did not finish before the time out.");
+		return 1;
 	}
 
 	ros::shutdown();
